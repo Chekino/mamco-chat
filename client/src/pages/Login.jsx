e30bd2aa13819c68a220eec431cb584f27b22ee6
Cheikh Ahmad Tidjani Diakite <diakite.cheikh200@gmail.com>
2023-12-21 02:12:48 +0000

    login user finish

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+import { AuthContext } from "../context/AuthContext";
 
 const Login = () => {
+  const { loginUser, loginInfo, updateLoginInfo, loginError, isLoginLoading } =
+    useContext(AuthContext);
   return (
     <>
-      <Form>
+      <Form onSubmit={loginUser}>
         <Row className="justify-content-md-center">
           <Col md={6}>
             <Stack gap={3}>
@@ -13,16 +16,26 @@ const Login = () => {
               <Form.Control
                 type="email"
                 placeholder="Entrez votre adresse E-mail"
+                onChange={(e) =>
+                  updateLoginInfo({ ...loginInfo, email: e.target.value })
+                }
               />
 
               <Form.Control
                 type="password"
                 placeholder="Entez un mot de passe"
+                onChange={(e) =>
+                  updateLoginInfo({ ...loginInfo, password: e.target.value })
+                }
               />
               <Button variant="success" type="submit">
-                Connexion
+                {isLoginLoading
+                  ? "Identification en cours ..."
+                  : "Identifiez-vous"}
               </Button>
-              <Alert variant="danger"> Erreur</Alert>
+              {loginError?.error && (
+                <Alert variant="danger"> {loginError?.message}</Alert>
+              )}
             </Stack>
           </Col>
         </Row>
