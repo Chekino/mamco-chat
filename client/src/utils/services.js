626a495b0167925457b6390720f066b7de16ffa4
Cheikh Ahmad Tidjani Diakite <diakite.cheikh200@gmail.com>
2024-01-05 17:23:41 +0000

    recupére chat

--- client/src/utils/services.js
+++ client/src/utils/services.js
@@ -29,3 +29,19 @@ export const postRequest = async (url, body) => {
 
   return data;
 };
+
+export const getRequest = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (!response.ok) {
+    let message = "Error an occured";
+
+    if (data?.message) {
+      message = data.message;
+    }
+
+    return { error: true, message };
+  }
+  return data;
+};
